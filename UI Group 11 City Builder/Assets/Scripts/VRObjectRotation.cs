5afe2e03af81353393a32ba51e4e9227ad9891e8
Samuel Ang <samuel.ang.prog@gmail.com>
2020-03-03 11:02:55 -0600

    Fixed cube rotation object

---
+++ UI Group 11 City Builder/Assets/Scripts/VRObjectRotation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRObjectRotation : MonoBehaviour
+{
+        //sensativity
+    [Header("Customization")]
+    public float xSensativity;
+    public float ySensativity;
+
+    public string axisToActivateRotation;
+
+    [Header("Helper Objects")]
+    public GameObject controlObject; //object which is used as reference for rotation
+    public GameObject objectToRotate;
+
+    //tracking variables
+    Vector3 cachedPosition; //cached position of the control object
+    bool rotating = true;
+
+    void Start(){
+        cachedPosition = controlObject.transform.localPosition;
+    }
+
+
+    void FixedUpdate(){
+        if(Input.GetAxisRaw(axisToActivateRotation) != 0){
+            rotating = true;
+        }else{
+            rotating = false;
+        }
+        if(rotating){
+            Vector3 delta = cachedPosition - controlObject.transform.localPosition;
+            objectToRotate.transform.Rotate(Vector3.up, delta.x * ySensativity, Space.World);
+            objectToRotate.transform.Rotate(Vector3.right, -delta.y * xSensativity, Space.World);
+            cachedPosition = controlObject.transform.localPosition;
+        }else{
+            cachedPosition = controlObject.transform.localPosition;
+        }
+    }
+
+
+
+}
