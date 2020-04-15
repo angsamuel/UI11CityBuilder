30de912687765a6bfe43e33acdbf32d8b2fa56e1
nodayshalleraseyou <nodayshalleraseyou>
2020-04-14 21:59:31 -0500

    Intigration and Build
    Added First build as an executable, integrated rotation, resource UI, and placement in "BuildScene"

---
+++ UI Group 11 City Builder/Assets/HighlightBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightBox : MonoBehaviour
+{
+    public GameObject model;
+    public Color greenColor;
+    public Color redColor;
+    public Color whiteColor;
+
+    void Start(){
+        Clear();
+    }
+
+    public void Clear(){
+        model.transform.localScale = new Vector3(0,500,0);
+    }
+    public void Highlight(TerrainCube tc, Building b){
+        transform.position = tc.transform.position;
+        model.transform.localScale = new Vector3(1,500,1);
+        if(b.BuildingConstraintsSatisfied(tc.x,tc.z)){
+            model.GetComponent<Renderer>().material.color = greenColor;
+        }else{
+            model.GetComponent<Renderer>().material.color = redColor;
+        }
+    }
+
+    public void Highlight(TerrainCube tc){
+        transform.position = tc.transform.position;
+        model.transform.localScale = new Vector3(1,500,1);
+        model.GetComponent<Renderer>().material.color = whiteColor;
+    }
+}
