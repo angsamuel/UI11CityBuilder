1ff7d9f050773ff84079b0e75e56d6f92abd7516
Razmin Riahi <razminr11@gmailcom>
2020-04-25 13:33:11 -0700

    Added Basic Timer
    -A basic TimerObject saved as a prefab.
    -Public values for minutes and seconds that can be modified as needed.
    -When the timer ends it logs the score (based on the pop value of the attached GameManager).

---
+++ UI Group 11 City Builder/Assets/TimerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour
+{
+    public int timeMins;
+    public int timeSecs;
+
+    public GameManager gm;
+
+    private float timeLeft;
+    private bool timesUp = false;
+    private int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = timeMins * 60 + timeSecs;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if(timeLeft <= 0 && !timesUp)
+        {
+            GameOver();
+            timesUp = true;
+        }
+    }
+
+    void GameOver()
+    {
+        score += gm.pop;
+        Debug.Log("Time's up!");
+        Debug.Log("Score = " + score);
+    }
+}
